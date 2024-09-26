cfc585805ea2d35da0a38ebd03ec9e594f1267e0
Parshav Singla <113877291+Parshav14@users.noreply.github.com>
2024-09-27 04:08:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import re
 
 # Configure pytesseract to use the Tesseract executable path if needed
 # Uncomment the following line if you need to set a specific Tesseract path
-pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
+pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
 # pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 def extract_text(image):
     """Extract text from the image using pytesseract."""
